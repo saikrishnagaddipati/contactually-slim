1302242b9fd8e691856bd6b9dead368ba73cbce9
adam-wanninger <wanninaj@miamioh.edu>
2015-08-25 14:13:18 -0400

    fixed bug, new records with data are created for every contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,32 +1,32 @@
 
 class Contact < ActiveRecord::Base
-  # attr_accessible :first_name, :last_name, :email_address
+
+  MAPPING = {
+      "First Name" => "first_name",
+      "Last Name" => "last_name",
+      "Email Address" => "email_address"
+  }
 
   def self.import(file)
     spreadsheet = open_spreadsheet(file)
     header = spreadsheet.row(1)
     (2..spreadsheet.last_row).each do |i|
       row = Hash[[header, spreadsheet.row(i)].transpose]
-      contact = find_by_id(row["id"]) || new
-      contact.attributes = row.to_hash.slice(*accessible_attributes)
-      # contact.attributes = row.to_hash.slice(*row.to_hash.keys)
-      contact.save!
+      # Convert the keys from the csv to match the database column names
+      row.keys.each { |k| row[ MAPPING[k] ] = row.delete(k) if MAPPING[k] }
+      # Remove company and phone number fields as these aren't in the database:
+      create(row.except!('Company Name', 'Phone Number'))
     end
   end
 
   def self.open_spreadsheet(file)
     case File.extname(file.original_filename)
-      when ".csv" then Roo::CSV.new(file.path, csv_options: {col_sep: ";"})
+      # You're using a tab seperated file, so specify seperator as a tab with \t
+      when ".csv" then Roo::CSV.new(file.path, csv_options: {col_sep: "\t"})
       when ".xls" then Roo::Excel.new(file.path)
       when ".xlsx" then Roo::Excelx.new(file.path)
       else raise "Unknown file type: #{file.original_filename}"
     end
-  end
-
-  private
-
-  def self.accessible_attributes
-    [:first_name, :last_name, :email_address]
   end
 
 end
